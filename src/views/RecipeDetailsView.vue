6eca2c73663f5d733241fb0bf4910911b3dc292b
kaleabbyh <kaleabbyh@gmail.com>
2023-09-28 04:32:12 +0300

    details updated

--- src/views/RecipeDetailsView.vue
+++ src/views/RecipeDetailsView.vue
@@ -12,31 +12,12 @@
         <p class="text-gray-700 mb-4">
           {{ recipe?.description }}
         </p>
-        <div class="flex justify-between">
-          <h2 class="text-2xl font-bold mb-2">Ingredients</h2>
-          <router-link
-            :to="`/addingredient/${recipe?.id}`"
-            class="bg-indigo-500 text-white py-1 px-2 rounded right-0 bottom-0 mr-4"
-          >
-            add ingredients
-          </router-link>
-        </div>
 
-        <ul
-          v-for="ingredient in recipe?.ingredients"
-          :key="ingredient.id"
-          class="list-disc ml-6"
-        >
-          <li>{{ ingredient.amount }} {{ ingredient.name }}</li>
-        </ul>
-
-        <div class="flex justify-between">
-          <h2 class="text-2xl font-bold mb-2">steps</h2>
-          <router-link
-            :to="`/addstep/${recipe?.id}`"
-            class="bg-indigo-500 text-white py-1 px-2 rounded right-0 bottom-0 mr-4"
-          >
-            add step
+        <br />
+        <div class="flex items-center">
+          <h2 class="text-2xl font-bold mb-2 mr-2">Steps</h2>
+          <router-link :to="`/addstep/${recipe?.id}`">
+            <img class="h-7 w-auto" src="../assets/images/add.png" alt="Logo" />
           </router-link>
         </div>
 
@@ -49,17 +30,19 @@
         </div>
       </div>
       <div class="w-full md:w-1/2 lg:w-1/3 px-4">
-        <h2 class="text-2xl font-bold mb-2">Nutritional Information</h2>
-        <ul class="list-disc ml-6">
-          <li>Calories: 180</li>
-          <li>Total Fat: 9g</li>
-          <li>Saturated Fat: 5g</li>
-          <li>Cholesterol: 30mg</li>
-          <li>Sodium: 120mg</li>
-          <li>Total Carbohydrate: 24g</li>
-          <li>Dietary Fiber: 1g</li>
-          <li>Sugars: 15g</li>
-          <li>Protein: 2g</li>
+        <div class="flex items-center">
+          <h2 class="text-2xl font-bold mb-2 mr-2">Ingredients</h2>
+          <router-link :to="`/addingredient/${recipe?.id}`">
+            <img class="h-7 w-auto" src="../assets/images/add.png" alt="Logo" />
+          </router-link>
+        </div>
+
+        <ul
+          v-for="ingredient in recipe?.ingredients"
+          :key="ingredient.id"
+          class="list-disc ml-6"
+        >
+          <li>{{ ingredient.amount }} {{ ingredient.name }}</li>
         </ul>
       </div>
     </div>
